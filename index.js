8951e1a01efc5169172b6969857819055c5831eb
Louis Mbuyu <louismbuyu17@gmail.com>
2019-07-29 22:44:33 -0500

    changes

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
+app.use(function (req,res,next) {
+    res.header('Access-Control-Allow-Origin',"http://localhost:3000");
+    res.header('Access-Control-Allow-Methods',"GET,PUT,POST,DELETE");
+    res.header('Access-Control-Allow-Headers',"Content-type");
+    next();
+});
+
 app.get('/', (req,res) => {
     return res.json({message: "hello"});
 });
